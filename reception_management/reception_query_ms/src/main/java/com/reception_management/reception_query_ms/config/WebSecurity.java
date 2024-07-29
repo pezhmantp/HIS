0cb0210a8c288ce3c7f4687ab77fb9e24ffc26f2
pezhmantp <pezhman.tabeshi@gmail.com>
2024-07-29 17:57:25 +0330

    Some changes added

---
+++ reception_management/reception_query_ms/src/main/java/com/reception_management/reception_query_ms/config/WebSecurity.java
@@ -0,0 +1,31 @@
+package com.reception_management.reception_query_ms.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new JwtRoleConverter());
+
+        http
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                .requestMatchers("/api/receptionQueries/**")
+                        .hasRole("receptionist")
+                .anyRequest().authenticated())
+                .oauth2ResourceServer(o -> o.jwt(j -> j.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+        return http.build();
+    }
+}
