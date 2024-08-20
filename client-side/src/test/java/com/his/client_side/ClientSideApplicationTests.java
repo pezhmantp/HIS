adf929ef8cc76958d1f7e2f93698cd25398373c7
pezhmantp <pezhman.tabeshi@gmail.com>
2024-08-20 10:40:44 +0330

    Spring MVC client-side app initialized

---
+++ client-side/src/test/java/com/his/client_side/ClientSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.his.client_side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
