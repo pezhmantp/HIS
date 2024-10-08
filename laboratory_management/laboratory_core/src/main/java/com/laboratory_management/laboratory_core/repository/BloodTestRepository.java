e952a9183b8f66549cfd63d2f08715c59689487b
pezhmantp <pezhman.tabeshi@gmail.com>
2024-10-08 12:15:36 +0330

    Some modifications on laboratory_core project

--- laboratory_management/laboratory_core/src/main/java/com/laboratory_management/laboratory_core/repository/BloodTestRepository.java
+++ laboratory_management/laboratory_core/src/main/java/com/laboratory_management/laboratory_core/repository/BloodTestRepository.java
@@ -1,9 +1,13 @@
 package com.laboratory_management.laboratory_core.repository;
 
 import com.laboratory_management.laboratory_core.model.BloodTest;
+import com.laboratory_management.laboratory_core.model.UrinalysisTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BloodTestRepository extends JpaRepository<BloodTest,String> {
+    List<BloodTest> findAllByVisitId(String visitId);
 }
--- laboratory_management/laboratory_core/src/main/java/com/laboratory_management/laboratory_core/repository/UrinalysisTestRepository.java
+++ laboratory_management/laboratory_core/src/main/java/com/laboratory_management/laboratory_core/repository/UrinalysisTestRepository.java
@@ -4,6 +4,9 @@ import com.laboratory_management.laboratory_core.model.UrinalysisTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UrinalysisTestRepository extends JpaRepository<UrinalysisTest,String> {
+    List<UrinalysisTest> findAllByVisitId(String visitId);
 }
