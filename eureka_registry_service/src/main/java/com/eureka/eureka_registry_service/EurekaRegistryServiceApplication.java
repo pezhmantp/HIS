c61f083bab39ca4b340e70de0fa70d349a6219eb
pezhmantp <pezhman.tabeshi@gmail.com>
2024-11-14 01:01:27 +0330

    eureka_registry_service project initialized

---
+++ eureka_registry_service/src/main/java/com/eureka/eureka_registry_service/EurekaRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.eureka_registry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ eureka_registry_service/src/test/java/com/eureka/eureka_registry_service/EurekaRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.eureka_registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
