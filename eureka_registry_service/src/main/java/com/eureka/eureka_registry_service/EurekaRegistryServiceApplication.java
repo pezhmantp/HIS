0d03bc0cb730fdc1b338a38671d96905620bb1c7
pezhmantp <pezhman.tabeshi@gmail.com>
2024-11-15 01:52:23 +0330

    Some changes added to the eureka_registry_service project

--- eureka_registry_service/src/main/java/com/eureka/eureka_registry_service/EurekaRegistryServiceApplication.java
+++ eureka_registry_service/src/main/java/com/eureka/eureka_registry_service/EurekaRegistryServiceApplication.java
@@ -2,8 +2,10 @@ package com.eureka.eureka_registry_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaRegistryServiceApplication {
 
 	public static void main(String[] args) {
