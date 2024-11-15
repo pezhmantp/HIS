bb2be8db69bf982c7e0445cdff0d475349c46432
pezhmantp <pezhman.tabeshi@gmail.com>
2024-11-15 04:44:15 +0330

    eureka-client added to the visit_management project

--- visit_management/visit_cmd_ms/src/main/java/com/visit_management/visit_cmd_ms/VisitCmdMsApplication.java
+++ visit_management/visit_cmd_ms/src/main/java/com/visit_management/visit_cmd_ms/VisitCmdMsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableMethodSecurity(prePostEnabled = true)
 @EnableJpaRepositories({"com.visit_management.visit_core"})
 @EntityScan(basePackages = {
--- visit_management/visit_query_ms/src/main/java/com/visit_management/visit_query_ms/VisitQueryMsApplication.java
+++ visit_management/visit_query_ms/src/main/java/com/visit_management/visit_query_ms/VisitQueryMsApplication.java
@@ -3,11 +3,13 @@ package com.visit_management.visit_query_ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories({"com.visit_management.visit_core"})
 @EntityScan(basePackages = {
 		"com.visit_management.visit_core.model",
