ed26712d44fc94a43775f6b37594adf3282d38e7
pezhmantp <pezhman.tabeshi@gmail.com>
2024-11-15 20:42:09 +0330

    eureka-client added to the reception_management project

--- reception_management/reception_cmd_ms/src/main/java/com/reception_management/reception_cmd_ms/ReceptionCmdMsApplication.java
+++ reception_management/reception_cmd_ms/src/main/java/com/reception_management/reception_cmd_ms/ReceptionCmdMsApplication.java
@@ -4,11 +4,13 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories({"com.reception_management.reception_core"})
 @EntityScan(basePackages = {
 		"com.reception_management.reception_core.model",
--- reception_management/reception_query_ms/src/main/java/com/reception_management/reception_query_ms/ReceptionQueryMsApplication.java
+++ reception_management/reception_query_ms/src/main/java/com/reception_management/reception_query_ms/ReceptionQueryMsApplication.java
@@ -3,9 +3,11 @@ package com.reception_management.reception_query_ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories({"com.reception_management.reception_core"})
 @EntityScan(basePackages = {
 		"com.reception_management.reception_core.model",
