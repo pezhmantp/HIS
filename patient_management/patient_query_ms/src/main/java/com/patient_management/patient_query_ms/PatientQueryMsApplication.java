c9c80880c951846bee69f1c7dc353a7131f10be2
pezhmantp <pezhman.tabeshi@gmail.com>
2024-11-15 05:33:27 +0330

    eureka-client added to the patient_management project

--- patient_management/patient_query_ms/src/main/java/com/patient_management/patient_query_ms/PatientQueryMsApplication.java
+++ patient_management/patient_query_ms/src/main/java/com/patient_management/patient_query_ms/PatientQueryMsApplication.java
@@ -3,11 +3,13 @@ package com.patient_management.patient_query_ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableMethodSecurity(prePostEnabled = true)
+@EnableDiscoveryClient
 @EnableJpaRepositories({"com.patient_management.patient_core"})
 @EntityScan(basePackages = {
 		"com.patient_management.patient_core.model",
