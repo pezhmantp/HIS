182a5a3991c769efd9a1db8b52985ac26eea7780
pezhmantp <pezhman.tabeshi@gmail.com>
2024-11-17 05:15:10 +0330

    eureka-client added to the laboratory_management project

--- laboratory_management/laboratory_cmd_ms/src/main/java/com/laboratory_management/laboratory_cmd_ms/LaboratoryCmdMsApplication.java
+++ laboratory_management/laboratory_cmd_ms/src/main/java/com/laboratory_management/laboratory_cmd_ms/LaboratoryCmdMsApplication.java
@@ -3,9 +3,11 @@ package com.laboratory_management.laboratory_cmd_ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories({"com.laboratory_management.laboratory_core"})
 @EntityScan(basePackages = {
 		"com.laboratory_management.laboratory_core.model",
