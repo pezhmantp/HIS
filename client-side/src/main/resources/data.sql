cf853f2677f01592ed2b4d100d4164c464ba72c9
pezhmantp <pezhman.tabeshi@gmail.com>
2025-02-09 19:35:20 +0330

    client-side project updated

---
+++ client-side/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO doctor_tbl (doctor_kc_username,doctor_full_name,department) VALUES
+		('doctor1','Mike brown',"General"),
+		('doctor2','Lily taylor',"Antenatal"),
+		('doctor3','Henry smith',"Neonatal");
